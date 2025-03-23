7a3386ac6762d9f815fa094480c2caf823022d5c
Ashan Dhanushka <ashandhanushka16@gmail.com>
2025-03-23 10:58:14 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 let count=0;
-
+let systemValue=Math.floor(Math.random() * 11);
 function Guess(){
     count++;
     if(count==3){
         document.getElementById("value").disabled = true;
         alert("Rounds are Over!");
+        return;
     }
     else if(document.getElementById("value").value == " "){
         alert("Error!You mus enter the number");
+        return;
     }
     else{
-    let systemValue=Math.floor(Math.random() * 101);
     let Value=parseInt(document.getElementById("value").value);
     let greater="Your number is greater than number";
     let lower="Your number is lower than number";
